3fbd159ba09b4b09691722f8807bd5f1aba8529b
Calvin Katt <katt.calvin@gmail.com>
2021-02-24 19:46:56 +0100

    Database Structure and Types

--- types/task.go
+++ types/task.go
@@ -5,5 +5,25 @@ type Task struct {
 	ID          uint64 `json:"id"`
 	Name        string `json:"name"`
 	Author      string `json:"author"`
-	DocumentURL string `json:"documentUrl"`
+	Description string `json:"description"`
+}
+
+//Subtask holds Information about one run of the programm
+type Subtask struct {
+	ID       uint64
+	Points   int
+	MainTask *Task
+	//In and output file paths are calculated on the fly by ID
+}
+
+//TasksPerUser holds all Tasks a given User can make
+type TasksPerUser struct {
+	Us    *User
+	Tasks []Task
+}
+
+type Submission struct {
+	ID     uint64
+	Author *User
+	//Source Code path is calculated by ID
 }
---
+++ types/user.go
@@ -0,0 +1,6 @@
+package types
+
+//User stores only access token for now
+type User struct {
+	Token string
+}
